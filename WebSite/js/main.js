da49f18bfda2e224130aa907c1d0ae90c45ecf88
sekine <sekine_y_529@msn.com>
2015-08-14 01:38:38 +0900

    雪の複数化 内部オブジェクト化

--- WebSite/js/main.js
+++ WebSite/js/main.js
@@ -4,6 +4,107 @@
     var LEFT_KEY_CODE = 37;
     var RIGHT_KEY_CODE = 39;
     var SCREEN_DIVS = 10;
+    var snows = [];
+    var snow_man_ob = null;
+
+    var SNOW_INCREMENT = 3;
+    var SNOW_INCREMENT_HEIGHT = 100;
+
+    // 雪オブジェクト
+    var snow = function (){
+        var img = new Image();
+        img.src = SNOW_IMAGE_SRC[0];
+
+        var x,y,underLimitPostion,rightLimitPostion;
+        var speed = 2;
+
+        // 雪の初期位置
+        this.setInitialPosition = function(containerWidth,containerHeight) {
+            underLimitPostion = containerHeight;
+            rightLimitPostion = containerWidth - img.width
+            x = getRandomPostion(rightLimitPostion);
+            y = 0;
+        }
+
+        this.move = function(){
+            y = y + speed;
+            if (y > underLimitPostion) {
+                x = getRandomPostion(rightLimitPostion);
+                y = 0;
+                img.src = SNOW_IMAGE_SRC[0];
+            }
+        }
+
+        // 画像表示
+        this.draw = function(ctx) {
+//            img.onload = function () {
+                ctx.drawImage(img, x, y);
+//            };
+        }
+
+        // 画像変更
+        this.doHit = function () {
+            img.src = SNOW_IMAGE_SRC[1];
+        }
+
+        this.getX = function () {
+            return x;
+        }
+        this.getY = function () {
+            return y;
+        }
+        this.getWidth = function () {
+            return img.width;
+        }
+        this.getHeight = function () {
+            return img.height;
+        }
+    }
+
+    // 雪ダルマオブジェクト
+    var snow_man = function(){
+        var img = new Image();
+        img.src = '/img/snow_man.png';
+
+        var x,y,right_limit_position;
+        var speed = 3;
+
+        this.initialize = function(containerWidth,containerHeight){
+            x = getCenterPostion(containerWidth, img.width);
+            y = containerHeight - img.height;
+            right_limit_position = getRightLimitPostion(containerWidth, img.width)
+        }
+
+        this.move = function (value) {
+            x += value * speed;
+            if (x < 0) {
+                x = 0
+            }
+            if (x > right_limit_position) {
+                x = right_limit_position;
+            }
+        }
+
+        this.draw = function (ctx) {
+//            img.onload = function () {
+                ctx.drawImage(img, x, y);
+//            };
+        }
+
+        this.getX = function () {
+            return x;
+        }
+        this.getY = function () {
+            return y;
+        }
+        this.getWidth = function () {
+            return img.width;
+        }
+        this.getHeight = function () {
+            return img.height;
+        }
+    }
+
     var SNOW_IMAGE_SRC = new Array();
     SNOW_IMAGE_SRC[0] = '/img/snow.png';
     SNOW_IMAGE_SRC[1] = '/img/sp_snow1.png';
@@ -15,8 +116,6 @@
     //プログラム全体で使用する変数
     var canvas = null;
     var ctx = null;
-    var img_snow = null;
-    var img_snow_man = null;
 
     // イベントハンドラ
     document.addEventListener("DOMContentLoaded", function () {
@@ -25,10 +124,10 @@
     document.addEventListener("keydown", function (event) {
         switch(event.keyCode){
             case LEFT_KEY_CODE:
-                key_value = -3;
+                key_value = -1;
                 break;
             case RIGHT_KEY_CODE:
-                key_value = +3;
+                key_value = +1;
                 break;
         }
     });
@@ -41,26 +140,20 @@
         canvas = document.getElementById('bg');
         // イベントハンドラ
         canvas.addEventListener("click", renderFrame);
-
         ctx = canvas.getContext('2d');
-        img_snow = new Image();
-        img_snow.src = SNOW_IMAGE_SRC[0];
-        img_snow._count = 0;
-        img_snow_man = new Image();
-        img_snow_man.src = '/img/snow_man.png';
 
-        // 画像読み込み完了時に表示する
-        img_snow.onload = function () {
-            img_snow._x = getCenterPostion(canvas.clientWidth, img_snow.width);
-            img_snow._y = 0;
-            ctx.drawImage(img_snow, img_snow._x, img_snow._y);
-        };
-        img_snow_man.onload = function () {
-            img_snow_man._x = getCenterPostion(canvas.clientWidth, img_snow_man.width);
-            img_snow_man._y = canvas.clientHeight - img_snow_man.height;
-            img_snow_man.right_limit_position = getRightLimitPostion(canvas.clientWidth, img_snow_man.width)
-            ctx.drawImage(img_snow_man, img_snow_man._x, img_snow_man._y);
+        var snow_ob = new snow();
+        snow_ob.setInitialPosition(canvas.clientWidth,canvas.clientHeight);
+        snows.push(snow_ob);
+
+        snow_man_ob = new snow_man();
+        snow_man_ob.initialize(canvas.clientWidth, canvas.clientHeight);
+
+        // 画像表示
+        for (var i = 0; i < snows.length ; i++){
+            snows[i].draw(ctx);
         }
+        snow_man_ob.draw(ctx);
 
     }
 
@@ -68,31 +161,37 @@
         // canvasをクリア
         ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
 
-        // 毎フレーム描画処理
-        img_snow._y += 2;
-        img_snow_man._x += key_value;
+        // 雪の毎フレーム描画処理
+        var snow_increment_flg = true;
+        for (var i = 0; i < snows.length; i++) {
+            snows[i].move();
+            if (isHit(snows[i], snow_man_ob)) {
+                // HITの表示
+                ctx.font = "bold 20px 'ＭＳ ゴシック'";
+                ctx.fillStyle = "red";
+                ctx.fillText("HIT", getCenterPostion(canvas.clientWidth, 140), 160);
 
-        // フレーム内限界判定
-        if (canvas.clientHeight < img_snow._y) {
-            img_snow = new Image();
-            img_snow.src = SNOW_IMAGE_SRC[0];
-            img_snow._count = 0;
-            img_snow._x = getInitialXPosition(canvas.clientWidth, img_snow.width );
-            img_snow._y = 0;
+                snows[i].doHit();
+            }
+
+            if (snows[i].getY() < SNOW_INCREMENT_HEIGHT) {
+                snow_increment_flg = false;
+            }
         }
-        if (img_snow_man._x > img_snow_man.right_limit_position) {
-            img_snow_man._x = img_snow_man.right_limit_position;
-        }
-        if (img_snow_man._x < 0) {
-            img_snow_man._x = 0;
+        if (snow_increment_flg) {
+            var snow_ob = new snow();
+            snow_ob.setInitialPosition(canvas.clientWidth, canvas.clientHeight);
+            snows.push(snow_ob);
         }
 
-        if (isHit(img_snow, img_snow_man)) {
-            doHit();
-        }
+        // 雪ダルさんの毎フレーム描画処理
+        snow_man_ob.move(key_value);
 
-        ctx.drawImage(img_snow, img_snow._x, img_snow._y);
-        ctx.drawImage(img_snow_man, img_snow_man._x, img_snow_man._y);
+        // 画像表示
+        for (var i = 0; i < snows.length ; i++) {
+            snows[i].draw(ctx);
+        }
+        snow_man_ob.draw(ctx);
 
         requestId = window.requestAnimationFrame(renderFrame);
     }
@@ -107,51 +206,16 @@
         return (containerWidth - itemWidth);
     }
 
+    function getRandomPostion(width) {
+        return width * Math.floor(Math.random(Date.now()) * (SCREEN_DIVS + 1)) / SCREEN_DIVS;
+    }
+
     // 当たり判定
     function isHit(targetA,targetB) {
-        return (((targetA._x <= targetB._x && targetA.width + targetA._x >= targetB._x)
-            || (targetA._x >= targetB._x && targetB._x + targetB.width >= targetA._x)) 
-            && ((targetA._y <= targetB._y && targetA.height + targetA._y >= targetB._y)
-                || (targetA._y >= targetB._y && targetB._y + targetB.height >= targetA._y)));
-    }
-
-    // 雪の初期位置
-    function getInitialXPosition(containerWidth, itemWidth) {
-        return (containerWidth - itemWidth) * Math.floor(Math.random(Date.now()) * (SCREEN_DIVS+1))/ SCREEN_DIVS;
-    }
-
-    function doHit() {
-        // HITの表示
-        ctx.font = "bold 20px 'ＭＳ ゴシック'";
-        ctx.fillStyle = "red";
-        ctx.fillText("HIT", getCenterPostion(canvas.clientWidth, 140), 160);
-
-        if (img_snow._count != 3) {
-            changeImage();
-        }
-    }
-
-    function changeImage() {
-        var temp_x = img_snow._x;
-        var temp_y = img_snow._y;
-        var temp_count = img_snow._count;
-        img_snow = new Image();
-        switch (temp_count) {
-            case 0:
-                img_snow.src = SNOW_IMAGE_SRC[1];
-                img_snow._count = 1;
-                break;
-            case 1:
-                img_snow.src = SNOW_IMAGE_SRC[2];
-                img_snow._count = 2;
-                break;
-            case 2:
-                img_snow.src = SNOW_IMAGE_SRC[3];
-                img_snow._count = 3;
-                break
-        }
-        img_snow._x = temp_x;
-        img_snow._y = temp_y;
+        return (((targetA.getX() <= targetB.getX() && targetA.getWidth() + targetA.getX() >= targetB.getX())
+            || (targetA.getX() >= targetB.getX() && targetB.getX() + targetB.getWidth() >= targetA.getX()))
+            && ((targetA.getY() <= targetB.getY() && targetA.getHeight() + targetA.getY() >= targetB.getY())
+                || (targetA.getY() >= targetB.getY() && targetB.getY() + targetB.getHeight() >= targetA.getY())));
     }
 
 })();
